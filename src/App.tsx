8a1663ea35eb400e1272dee33b749b712e387800
Mateusz <mateusz.grzybowski98@gmail.com>
2023-02-28 12:54:23 +0100

    Added styles and error funcionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
@@ -12,8 +12,18 @@ interface Movie {
 function App() {
   const [search, setSearch] = useState('');
   const [movie, setMovie] = useState<Movie | null>(null);
+  const [error, setError] = useState(false);
+  const [wrongMovie, setWrongMovie] = useState(false);
 
   const showMovie = () => {
+    if (search === '') {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 2000);
+      return;
+    }
+
     axios
       .get(`https://imdb-api.com/en/API/Search/k_fq7cg5zc/${search}`)
       .then((response) => {
@@ -32,19 +42,21 @@ function App() {
 
   return (
     <div className="App">
-      <form className="flex justify-center" onSubmit={submit}>
+      <h1 className="text-center text-3xl">Movie App</h1>
+      <form className="flex justify-center my-2" onSubmit={submit}>
         <input
           className="
         text-center
         border-2
         border-gray-300
         bg-white
+        outline-none
         "
           type="text"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <button className="border p-1" onClick={showMovie}>
+        <button className="border p-1 hover:bg-gray-200" onClick={showMovie}>
           Search
         </button>
       </form>
@@ -52,10 +64,14 @@ function App() {
         <div key={movie.id}>
           <h1 className="text-center">{movie.title}</h1>
           <p>{movie.Year}</p>
-          <img src={movie.image} alt="movie poster" />
+          <img
+            src={movie.image}
+            alt="movie poster"
+            className="sm:w-[400px] h-[400px] m-auto"
+          />
         </div>
       ) : null}
-      {movie === null ? <p>No movie found.</p> : null}
+      {error ? <p className="text-center">Please enter a movie name.</p> : null}
     </div>
   );
 }
